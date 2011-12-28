d701b4c6e2527437f51eb94e7d40407af804f47c
john <john@john-desktop.(none)>
2011-12-28 20:43:16 +0900

    rewrite test001.cpp

--- helloworld/test001.cpp
+++ helloworld/test001.cpp
@@ -1,12 +1,7 @@
 #include<iostream>
-#include<time.h>
 
 int main(int argc,char* argv[]){
-    clock_t start,end;
-    start=clock();
     std::cout<<"hello world!"<<std::endl;
-    end=clock();
-    std::cout<<"time:"<<end-start<<"ms"<<std::endl;
     return 0;
 }
 
