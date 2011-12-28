6e77a3d1978731340655f31a5112afa087aad932
john <john@john-desktop.(none)>
2011-12-28 18:40:50 +0900

    add test001.cpp

--- template/test001.cpp
+++ template/test001.cpp
@@ -3,25 +3,25 @@
 
 template <class T> void swapargs(T& a,T& b){
     T temp=a;
-	a=b;
-	b=temp;
+    a=b;
+    b=temp;
 }
 
 int main(int argc,char* argv[]){
     int x=1;
-	int y=2;
+    int y=2;
 
     std::cout<<"x="<<x<<", y="<<y<<std::endl;
-	swapargs(x,y);
+    swapargs(x,y);
     std::cout<<"x="<<x<<", y="<<y<<std::endl;
 
     std::string str1="str1";
-	std::string str2="str2";
+    std::string str2="str2";
 
     std::cout<<"str1="<<str1<<", str2="<<str2<<std::endl;
-	swapargs(str1,str2);
+    swapargs(str1,str2);
     std::cout<<"str1="<<str1<<", str2="<<str2<<std::endl;
 
-	return 0;
+    return 0;
 }
 
