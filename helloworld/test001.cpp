c2691d0cdde99843ca3c229cc22dc799c80f13ae
john <john@john-desktop.(none)>
2011-12-28 18:37:23 +0900

    add test001.cpp

--- helloworld/test001.cpp
+++ helloworld/test001.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 #include<time.h>
 
-int main(){
+int main(int argc,char* argv[]){
     clock_t start,end;
-	start=clock();
+    start=clock();
     std::cout<<"hello world!"<<std::endl;
-	end=clock();
-	std::cout<<"time:"<<end-start<<"ms"<<std::endl;
-	return 0;
+    end=clock();
+    std::cout<<"time:"<<end-start<<"ms"<<std::endl;
+    return 0;
 }
 
